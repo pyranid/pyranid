80cfa152aaa2f08c245a546d3e930c573aec2a7c
Mark Allen <mark.a.allen@gmail.com>
2023-06-27 22:05:21 -0400

    Missed in last commit

--- src/main/java/com/pyranid/package-info.java
+++ src/main/java/com/pyranid/package-info.java
@@ -49,9 +49,9 @@
  * ResultSetMapper resultSetMapper = new DefaultResultSetMapper(instanceProvider) {
  *   &#064;Nonnull
  *   &#064;Override
- *   public &lt;T&gt; T map(&#064;Nonnull StatementContext&lt;T&gt; statementContext,
- *                    &#064;Nonnull ResultSet resultSet,
- *                    &#064;Nonnull Class&lt;T&gt; resultSetRowType) {
+ *   public &lt;T&gt; Optional&lt;T&gt; map(&#064;Nonnull StatementContext&lt;T&gt; statementContext,
+ *                              &#064;Nonnull ResultSet resultSet,
+ *                              &#064;Nonnull Class&lt;T&gt; resultSetRowType) {
  *     return super.map(statementContext, resultSet, resultSetRowType);
  *   }
  * };
